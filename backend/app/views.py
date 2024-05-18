af0c2389d1f98bb7d887663feeff4249bd419717
SleimaD <sleima@Ordinateur-portable-de-Sleima.local>
2024-05-19 00:13:36 +0200

    add of views to display elements

--- backend/app/views.py
+++ backend/app/views.py
@@ -1,3 +1,40 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Continent, Pays, Equipe, Joueur, Role
+from .serializers import ContinentSerializer, PaysSerializer, EquipeSerializer, JoueurSerializer, RoleSerializer
 
-# Create your views here.
+
+#! afficher les models
+@api_view(['GET'])
+def continent_list(request):
+    continents = Continent.objects.all()
+    serializer = ContinentSerializer(continents, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def pays_list(request):
+    pays = Pays.objects.all()
+    serializer = PaysSerializer(pays, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def equipe_list(request):
+    equipes = Equipe.objects.all()
+    serializer = EquipeSerializer(equipes, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def joueur_list(request):
+    joueurs = Joueur.objects.all()
+    serializer = JoueurSerializer(joueurs, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def role_list(request):
+    roles = Role.objects.all()
+    serializer = RoleSerializer(roles, many=True)
+    return Response(serializer.data)
+
+
